e707e751a75f081afec26bf13848048b6a222707
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-11-18 19:10:38 +0200

    start

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="">
+        ABDELHADY <br />
+        <button className="btn btn-primary">hady</button>
+      </div>
+    );
+  }
+}

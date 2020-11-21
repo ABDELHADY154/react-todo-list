eeffe780041994df4dbeb02f60a22d96d3ac573d
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-11-21 11:17:45 +0200

    .

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div className="">
-        ABDELHADY <br />
-        <button className="btn btn-primary">hady</button>
+      <div className="container-fluid align-center justify-content-center">
+        <div className="container">Todo - List</div>
       </div>
     );
   }
